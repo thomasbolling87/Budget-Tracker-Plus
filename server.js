aa748ce4a5e0fad928a7c758c34b361137a52309
Thomas Bolling <Thomas.Bolling.87@gmail.com>
2021-07-16 22:27:34 -0500

    updated server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:/Budget-Tracker', {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
